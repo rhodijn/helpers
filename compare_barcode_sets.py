733a1ec9a116493af2afb331160f6bf4e26f65b4
Rhodijn <roger.zollinger@proton.me>
2025-08-26 10:44:47 +0200

    barcode liste script generated

--- compare_barcode_sets.py
+++ compare_barcode_sets.py
@@ -40,32 +40,21 @@ else:
     print(f"Vergleich von {files[0]} und {files[1]}")
     with open(filepath + files[0]) as csv_liste_1:
         liste_reader = list(csv.reader(csv_liste_1, delimiter=delim))
-        for k, v in enumerate(liste_reader[0]):
-            if re.search(searchterm, v.lower()):
-                break
         for el in liste_reader:
-            if not re.search(searchterm, el[k].lower()):
-                set_1.add(el[k])
+            if not re.search(searchterm, el[0].lower()):
+                set_1.add(el[0])
+                print(len(set_1))
 
     with open(filepath + files[1]) as csv_liste_2:
         liste_reader = list(csv.reader(csv_liste_2, delimiter=delim))
-        for k, v in enumerate(liste_reader[0]):
-            if re.search(searchterm, v.lower()):
-                break
-        for el in liste_reader:
-            if not re.search(searchterm, el[k].lower()):
-                set_2.add(el[k])
+        for row in liste_reader[0]:
+            set_2.add(row)
 
-    for el in set_1:
-        if el not in set_2:
-            one_not_two.append(el)
-
-    for el in set_2:
-        if el not in set_1:
-            one_not_two.append(el)
-
+    print(set_1)
+"""
     print(f"{len(set_1)} Einträge in: {files[0]}")
     print(f"{len(set_2)} Einträge in: {files[1]}")
 
     print(f"{len(one_not_two)} Einträge nur in: {files[0]}")
     print(f"{len(two_not_one)} Einträge nur in: {files[1]}")
+"""
