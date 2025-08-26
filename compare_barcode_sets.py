c588b84fc9aba436f18636e8e911e22ef41315d4
Rhodijn <roger.zollinger@proton.me>
2025-08-26 14:52:01 +0200

    updated script with output

--- compare_barcode_sets.py
+++ compare_barcode_sets.py
@@ -18,13 +18,10 @@ import csv, os, re
 delim = ';'
 files = []
 filepath = 'files/'
-liste = []
-one_not_two = []
-two_not_one = []
-parts = []
 searchterm = 'barcode'
 set_1 = set()
 set_2 = set()
+set_3 = set()
 
 
 files = os.listdir(filepath)
@@ -38,24 +35,28 @@ if len(files) != 2:
     print(f"Der Vergleich benötigt 2 csv-Dateien, {len(files)} gefunden.")
 else:
     print(f"Vergleich von {files[0]} und {files[1]}")
-    with open(filepath + files[0]) as csv_liste_1:
-        liste_reader = list(csv.reader(csv_liste_1, delimiter=delim))
-        for el in liste_reader:
+
+    with open(filepath + files[0]) as csv_file:
+        reader = csv.reader(csv_file, delimiter=delim)
+        for el in reader:
             if not re.search(searchterm, el[0].lower()):
                 set_1.add(el[0])
-        print(len(set_1))
 
-    with open(filepath + files[1]) as csv_liste_2:
-        liste_reader = list(csv.reader(csv_liste_2, delimiter=delim))
-        for el in liste_reader:
+    with open(filepath + files[1]) as csv_file:
+        reader = csv.reader(csv_file, delimiter=delim)
+        for el in reader:
             if not re.search(searchterm, el[0].lower()):
                 set_2.add(el[0])
-        print(len(set_2))
 
-"""
+    with open(f"{filepath}output/union.csv", 'w', newline='\n') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow('barcode')
+        writer.writerow(set_1.union(set_2))
+
     print(f"{len(set_1)} Einträge in: {files[0]}")
     print(f"{len(set_2)} Einträge in: {files[1]}")
 
-    print(f"{len(one_not_two)} Einträge nur in: {files[0]}")
-    print(f"{len(two_not_one)} Einträge nur in: {files[1]}")
-"""
+    print(f"{len(set_1.union(set_2))} Einträge total in beiden Dateien")
+    print(f"{len(set_1.intersection(set_2))} gemeinsame Einträge in beiden Dateien vorhanden")
+    print(f"{len(set_1.difference(set_2))} Einträge ausschliesslich in {files[0]}")
+    print(f"{len(set_2.difference(set_1))} Einträge ausschliesslich in {files[1]}")
