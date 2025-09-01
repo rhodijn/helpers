f9d9a4c817778b94f84effbab95fc5c6527de4cf
Rhodijn <roger.zollinger@proton.me>
2025-09-01 11:19:36 +0200

    version 1 of chemie-script finished

--- scan_items.py
+++ scan_items.py
@@ -9,10 +9,8 @@
 #   ##                 ##
 #     ###################
 
-# This script compares two csv-files and reports the differences.
 
-
-import csv, os, re
+import os
 import pandas as pd
 
 barcodes = []
@@ -42,18 +40,21 @@ df_wae_c = pd.DataFrame(pd.read_csv(f"{filepath}/{files[ind]}", dtype=str, sep='
 
 df_wae_c['strichcode'] = df_wae_c['strichcode'].str.upper()
 
+print('\n\t[=========================]\n\t[ Archivbestand Chemie    ]')
+print('\t[ version 1, by zolo      ]\n\t[=========================]\n')
+
 while True:
-    barcodes.append(input('scan items (\'q\' to exit): '))
+    barcodes.append(input('scan item (or press \'q\' to exit): '))
     if barcodes[-1] == 'q':
-        print('\n\t\t\t[=========]\n\t\t\t[  BYE    ]\n\t\t\t[=========]\n')
+        print('\n\t[=========]\n\t[ bye     ]\n\t[=========]\n')
         break
     else:
         df_scanned = df_wae_c[df_wae_c['strichcode'].isin(barcodes)]
         if df_scanned[df_scanned['hicr'].notna()].size > 0:
-            print('\n\t\t\t[=========]')
-            print('\t\t\t[  KEEP!  ]\n\t\t', end='')
+            print('\n\t\t\t\t[=========]')
+            print('\t\t\t\t[ KEEP!   ]\n\t\t\t', end='')
         else:
             print('\n\t[=========]')
-            print('\t[  THROW  ]')
+            print('\t[ THROW   ]')
     print('\t[=========]\n')
     barcodes.pop()
