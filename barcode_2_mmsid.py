2189f63dea5d9a58245f212318d7c473154ec6f7
Rhodijn <roger.zollinger@proton.me>
2025-09-08 13:33:26 +0200

    update script for mms ids

--- barcode_2_mmsid.py
+++ barcode_2_mmsid.py
@@ -14,13 +14,37 @@
 # this script find the mms ids to the items in a list of barcodes
 
 
-import math, os
+import json, os, requests
 import pandas as pd
-
+from dotenv import dotenv_values
 
 barcodes = []
 files = []
 filepath = 'files'
+secrets = dotenv_values('.env')
+
+
+def api_request(method: str, value: str, frmt: str, par_1: str, par_2='') -> tuple:
+    """
+    perform an api request and return the answer
+
+    parameters:
+    method: str = api request method (GET, PUT, POST, ...)
+    value: str = item id
+    frmt: str = format (json, xml)
+    param_1: str = api parameter 1
+    param_2: str = api parameter 2
+
+    returns:
+    tuple = (req: str, response: requests.models.Response)
+    """
+    response = False
+
+    if method == 'get':
+        req = f"{secrets['API_URL']}{par_1}{value}{par_2}&apikey={secrets['API_KEY']}&format={frmt}"
+        response = requests.get(req)
+
+    return req, response
 
 
 # scan folder and exclude subfolders
@@ -40,9 +64,14 @@ print(files[ind])
 df_wae_loeschen = pd.DataFrame(pd.read_csv(f"{filepath}/{files[ind]}", dtype=str, sep=';'))
 df_wae_loeschen['barcode'] = df_wae_loeschen['barcode'].str.upper()
 
-print(len(df_wae_loeschen))
 
 for i, el in enumerate(df_wae_loeschen['barcode']):
-    print(el)
+    req, get_iz_mmsid = api_request('get', el, 'json', 'items?item_barcode=')
+    data = json.loads(get_iz_mmsid.content.decode(encoding='utf-8'))
+    mmsid_iz = data['bib_data']['mms_id']
+    df_wae_loeschen.loc[i, 'mms id'] = mmsid_iz
 
-print(len(df_wae_loeschen))
+try:
+    df_wae_loeschen.to_csv('files/output/wae_loeschen.csv', index=False, sep=';')
+except Exception as e:
+    print(f"an error occurred: {e}")
